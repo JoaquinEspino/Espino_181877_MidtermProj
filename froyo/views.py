11af5350ab75613715f40e34b37c5b73bc5daf37
JoaquinEspino <joaquin.espino@obf.ateneo.edu>
2020-03-16 08:05:11 +0800

    HTML files and froyo url

--- froyo/views.py
+++ froyo/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 
 # Create your views here.
+
+class IngredientsListView(ListView):
+	template_name = 'ingredients_list.html'
+
+class IngredientsDetailView(DetailView):
+	template_name = 'ingredients_detail.html'
+
+class IngredientsUpdateView(UpdateView):
+	template_name = 'ingredients_update_form.html'
+
+class IngredientsCreateView(CreateView):
+	template_name = 'ingredients_create_form.html'
+
+class RecipesListView(ListView):
+	template_name = 'recipes_list.html'
+
+class RecipesDetailView(DetailView):
+	template_name = 'recipes_detail.html'
+
+class RecipesUpdateView(UpdateView):
+	template_name = 'recipes_update_form.html'
+
+class RecipesCreateView(ListView):
+	template_name = 'recipes_create_form.html'
+
+class OrdersListView(ListView):
+	template_name = 'orders_list.html'
+
+class OrdersDetailView(ListView):
+	template_name = 'orders_detail.html'
+
+class OrdersUpdateListView(ListView):
+	template_name = 'orders_update_form.html'
+
+class OrdersCreateView(ListView):
+	template_name = 'orders_create_form.html'
+
